42625d45293e25b37aea11ff7dca115e8f116432
Sara Yeo <sara.yeo@utah.edu>
2021-01-25 17:25:16 -0700

    revising LA.3

--- COMM-3710/LA/LA.3.R
+++ COMM-3710/LA/LA.3.R
@@ -8,17 +8,32 @@ library(descr)
 ## Set working directory
 setwd("C:/Users/SaraK/Downloads/sarakyeo.github.io/COMM-3710/LA")
 
-## 1: Load dataset
+## 1. Load dataset
 hdata <- read.csv("HELP.csv") # load HELP.csv
         # loaded data of 453 observations of 27 variables
 
-## 2: Make frequency tables for sex and d1
+## 2. Make frequency tables for sex and d1
 freq(hdata$sex) # freq table for sex
 freq(hdata$d1) # freq table for d1
 
-## 3: Subset data to incl substance == cocaine and over 40 years old
+## 2a. How many patients in the study are female?
+freq(hdata$sex) # 107 female patients
+
+## 2b. How many have never been hospitalized for medical problems?
+freq(hdata$d1) # 92 have never been hospitalized for medical problems
+
+## 2c. What percentage of patients in the study have been hospitalized fewer than 5 times?
+lessfive <- (92 + 120 + 92 + 36 + 37 + 18) / 453
+lessfive # 87.2%
+
+
+## 3. Subset data to incl substance == cocaine and over 40 years old
 hsub <- subset(hdata, substance == "cocaine" & age >= 40) # subset data
         # contains 27 obs of 27 var
 
-## 4: Make freq table based on subset
+## 4. Make freq table based on subset
 freq(hsub$sex) # freq table for sex (subset of data)
+
+## 4a. 27 patients are at least 40 years old and have cocaine listed as his/her primary abuse substance.
+
+## 4b. 20 patients are at least 40 years old, have cocaine listed as his/her primary abuse substance, and are male.
