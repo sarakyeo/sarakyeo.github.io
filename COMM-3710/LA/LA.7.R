dc842b92a655666e2749ae67da412e27b2db3e30
Sara Yeo <sara.yeo@utah.edu>
2021-02-03 10:03:11 -0700

    revising LA.7

--- COMM-3710/LA/LA.7.R
+++ COMM-3710/LA/LA.7.R
@@ -25,19 +25,47 @@ cps %>%
         subset(sex == "F") %>% 
         summarise(M = mean(wage, na.rm = TRUE)) # $7.88
 
-# or ...
+# or...
 
 mwage <- mean(cps$wage[cps$sex == "M"])
 fwage <- mean(cps$wage[cps$sex == "F"])
 
 
-## 3: 
+## 3: Conduct appropriate statistical test
+cps %>% 
+        t_test(., wage ~ sex, var.equal = TRUE)
+
+# or...
+
 t.test(cps$wage ~ cps$sex, var.equal = TRUE)
 
-## 4
-res.cor <- cor.test(cps$wage, cps$exper, method = "pearson")
-res.cor
+## 3a: t-test to compare continuous DV and categorical IV.
+## 3b: t = -4.84
+## 3c: p < .05; The mean wage earned per hour for females is significantly lower than that of males.
 
-## 5
+
+## 4: Conduct appropriate statistical test
+freq(cps$exper)
+cps %>% 
+        cor_test(., exper, wage, method = "pearson")
+
+# or...
+
+cor.test(cps$wage, cps$exper, method = "pearson")
+
+## 4a: Pearson's correlation to compare two continuous variables.
+## 4b: r = 0.087
+## 4c: p = .044; There is a weak positive correlation (r = .087) between number of years of work experience and wages earned per hour.
+
+
+## 5: Conduct appropriate statistical test
 tbl <- table(cps$sector, cps$satisfaction)
 chisq.test(tbl)
+
+# or...
+
+chisq_test(tbl)
+
+## 5a: Chi-squared test 
+## 5b: X2 = 3.65 
+## 5c: p = .819; There is no significant difference in job satisfaction by sector.
