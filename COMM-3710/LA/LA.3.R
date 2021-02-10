cfc846244e31151f65f795e696c489d955fcdb76
Sara Yeo <sara.yeo@utah.edu>
2021-02-10 11:04:16 -0700

    Corrected typo in LA.5

--- COMM-3710/LA/LA.3.R
+++ COMM-3710/LA/LA.3.R
@@ -22,7 +22,8 @@ freq(hdata$sex) # 107 female patients
 ## 2b. How many have never been hospitalized for medical problems?
 freq(hdata$d1) # 92 have never been hospitalized for medical problems
 
-## 2c. What percentage of patients in the study have been hospitalized fewer than 5 times?
+## 2c. What percentage of patients in the study have been hospitalized fewer
+##     than 5 times?
 lessfive <- (92 + 120 + 92 + 36 + 37 + 18) / 453
 lessfive # 87.2%
 
@@ -34,6 +35,8 @@ hsub <- subset(hdata, substance == "cocaine" & age >= 40) # subset data
 ## 4. Make freq table based on subset
 freq(hsub$sex) # freq table for sex (subset of data)
 
-## 4a. 27 patients are at least 40 years old and have cocaine listed as his/her primary abuse substance.
+## 4a. 27 patients are at least 40 years old and have cocaine listed as his/her
+##     primary abuse substance.
 
-## 4b. 20 patients are at least 40 years old, have cocaine listed as his/her primary abuse substance, and are male.
+## 4b. 20 patients are at least 40 years old, have cocaine listed as his/her
+##     primary abuse substance, and are male.
