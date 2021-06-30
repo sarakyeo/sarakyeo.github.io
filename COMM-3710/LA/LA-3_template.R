285f21043cc6fd4f35320e7ce0d7fa9b01d39c1f
sarakyeo <sara.yeo@utah.edu>
2021-06-30 13:53:30 -0600

    preparing COMM 3710 for fall 2021

---
+++ COMM-3710/LA/LA-3_template.R
@@ -0,0 +1,85 @@
+# Name: Louise Belcher
+# Student ID: u0000001
+# Date: MM/DD/YYYY
+# COMM 3710 Assignment: LA.3
+
+
+# Installing and Loading Packages -----------------------------------------
+# Install tidyverse package
+install.packages()
+# Install magrittr package
+# Install descr package
+# Install rstatix package
+
+# Load tidyverse package
+library()
+# Load magrittr package
+# Load descr package
+# Load rstatix package
+
+
+# Set working directory ---------------------------------------------------
+
+
+
+
+# Check out c() function in R ---------------------------------------------
+# Pull up help page for c()
+
+# The c() function... [add description here]
+
+
+
+# Entering data -----------------------------------------------------------
+# Enter data into Aname
+Aname <- c()
+
+# Enter data into Ateam
+Ateam <- c()
+
+# Enter data into ppg
+ppg <- c()
+
+# Enter data into pts
+pts <- c()
+
+
+
+# Check and combine -------------------------------------------------------
+# Check Ateam, Aname, ppg, and pts
+
+# Combine objects into tibble
+mydata <- tibble()
+# If you need help with the tibble function, pull up the help documentation.
+
+
+
+# Examine data types in mydata tibble -------------------------------------
+# First, pull up and read documentation for class() 
+
+# The class() function ... [add description here]
+
+# Check class of each column in mydata tibble
+
+# Ateam is ...
+# Aname is ...
+# ppg is ...
+# pts is ...
+
+
+# Run and complete learnr tutorial ----------------------------------------
+# Install learnr package
+# Load learnr package
+
+
+# Answers to Step 10 ------------------------------------------------------
+# 10a. 
+# 10b. 
+# 10c. 
+# 10d. 
+
+
+
+
+
+
