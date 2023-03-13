83bcc83f16cdfc344dc94e9014ddfb95b4a03ce4
Sara K Yeo <sarakyeo@gmail.com>
2023-03-13 12:53:07 -0600

    working on data viz for 7370

--- COMM-7370/data-viz.R
+++ COMM-7370/data-viz.R
@@ -22,3 +22,40 @@ ggplot(
         data = penguins,
         mapping = aes(x = flipper_length_mm, y = body_mass_g)
 ) # What happens when you run this command?
+
+ggplot(
+        data = penguins,
+        mapping = aes(x = flipper_length_mm, y = body_mass_g)
+) +
+        geom_point() # Now what? Describe the graph. What is it plotting?
+# Look at the data again, if necessary.
+
+ggplot(
+        data = penguins,
+        mapping = aes(x = flipper_length_mm,
+                      y = body_mass_g,
+                      color = species)
+) +
+        geom_point() # So fancy
+
+# Add smoothed lines
+ggplot(
+        data = penguins,
+        mapping = aes(x = flipper_length_mm,
+                      y = body_mass_g,
+                      color = species)
+) +
+        geom_point() +
+        geom_smooth(method = "lm")
+
+# But wait... we want the color for species and only a single smoothed line
+ggplot(
+        data = penguins,
+        mapping = aes(x = flipper_length_mm,
+                      y = body_mass_g)
+) +
+        geom_point(mapping = aes(
+                color = species,
+                shape = species
+        )) +
+        geom_smooth(method = "lm")
