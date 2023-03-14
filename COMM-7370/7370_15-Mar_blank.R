70849278629d657df936b30f8247dff0f455c421
Sara K Yeo <sarakyeo@gmail.com>
2023-03-14 11:11:31 -0600

    added practice data codebook and blank R script for students

---
+++ COMM-7370/7370_15-Mar_blank.R
@@ -0,0 +1,221 @@
+# R4DS: Data visualization ------------------------------------------------
+# https://r4ds.hadley.nz/data-visualize.html
+
+# Install/Load packages: tidyverse, ggthemes, palmerpenguins, nycflights13
+
+
+# Check out the penguins data!
+
+
+# Learn about the variables in the penguins data frame
+
+
+
+## ggplot: Step-by-step ----------------------------------------------------
+## Follow the Data visualization section
+ggplot(
+  data = penguins
+) # What happens when you run this command?
+
+ggplot(
+  data = penguins,
+  mapping = aes(x = flipper_length_mm, y = body_mass_g)
+) # What happens when you run this command?
+
+ggplot(
+  data = penguins,
+  mapping = aes(x = flipper_length_mm, y = body_mass_g)
+) +
+  geom_point() # Now what? Describe the graph. What is it plotting?
+# Look at the data again, if necessary.
+
+ggplot(
+  data = penguins,
+  mapping = aes(
+    x = flipper_length_mm,
+    y = body_mass_g,
+    color = species
+  )
+) +
+  geom_point() # So fancy
+
+# Add smoothed lines
+ggplot(
+  data = penguins,
+  mapping = aes(
+    x = flipper_length_mm,
+    y = body_mass_g,
+    color = species
+  )
+) +
+  geom_point() +
+  geom_smooth(method = "lm")
+
+# But wait... we want the color for species and only a single smoothed line
+ggplot(
+  data = penguins,
+  mapping = aes(
+    x = flipper_length_mm,
+    y = body_mass_g
+  )
+) +
+  geom_point(mapping = aes(
+    color = species,
+    shape = species
+  )) +
+  geom_smooth(method = "lm")
+
+# Changing details...
+ggplot(
+  data = penguins,
+  mapping = aes(
+    x = flipper_length_mm,
+    y = body_mass_g
+  )
+) +
+  geom_point(mapping = aes(
+    color = species,
+    shape = species
+  )) +
+  geom_smooth(method = "lm") +
+  labs(
+    title = "Body mass and flipper length",
+    subtitle = "Dimensions for Adelie, Chinstrap, and Gentoo Penguins",
+    x = "Flipper length (mm)",
+    y = "Body mass (g)",
+    color = "Species",
+    shape = "Species"
+  ) +
+  scale_color_colorblind()
+
+
+## 2.2.5 Exercises ---------------------------------------------------------
+# 1: 
+
+# 2: 
+
+# 3:
+
+# 4:
+
+# 5: 
+
+# 6:
+
+# 7:
+
+# 8:
+
+# 9:
+
+# 10:
+
+
+## 2.4.3 Exercises ---------------------------------------------------------
+# 1: 
+
+# 2: 
+
+# 3: 
+
+# 4:
+
+
+## 2.5.5 Exercises ---------------------------------------------------------
+# 1: 
+
+# 2: 
+
+# 3: 
+
+# 4: 
+
+# 5:
+
+# 6: 
+
+# 7:
+
+
+## 2.6.1 Exercises ---------------------------------------------------------
+# 1: 
+
+# 2: 
+
+
+# Data management tutorial ------------------------------------------------
+## Setting Preferences in R ------------------------------------------------
+# Tools --> Global Options --> Appearance
+# Rainbow parentheses
+# Shortcuts
+## Comments: Ctrl + Shift + C
+## Foldable code: Ctrl + Shift + R
+## Run code: Ctrl + Enter
+## Arrow: Alt + -
+
+
+## Setting Up R for Assignments --------------------------------------------
+# Creating R scripts
+# Installing and loading packages
+# install.packages("tidyverse")
+# install.packages("summarytools")
+# install.packages("rstatix")
+# install.packages("magrittr")
+# install.packages("psych")
+[write code here]
+
+# Setting working directory
+[paste code here]
+
+
+## Best Practices ----------------------------------------------------------
+# What does your future self need to know in order to understand this analysis?
+# Writing foldable code makes it easier to navigate a lot of code.
+# And your analysis is likely to consist of a LOT of code.
+# Always install and load packages at the beginning of your code.
+# R is case-sensitive while SPSS is not--be aware of this.
+# Data and syntax redundancy--what to save?
+
+
+## Pipe Operator -----------------------------------------------------------
+# https://r4ds.had.co.nz/pipes.html
+# We will work through the four piping alternatives in the document.
+# Shortcut key: Ctrl + Shift + M
+# Nouns vs. verbs in coding--easier to think in verbs instead of nouns.
+
+
+## Reading Data into R -----------------------------------------------------
+[write code here]
+
+# Cleaning data
+# Use gc to filter data--we only want R for whom gc = 1
+[write code here]
+
+## Types of Data and Levels of Measurement ---------------------------------
+# What is C1? Where do I find info about C1? Why do I need to do this?
+[write code here]
+
+# Where do I find info re: V1? What type of variable is it?
+[write code here]
+
+# Calculate age of respondents. Why is this important?
+[write code here]
+
+
+## Missing Data ------------------------------------------------------------
+# Recode missing data as NA in R.
+
+
+## Descriptive Statistics --------------------------------------------------
+# Uses summarytools package
+# Find descriptive statistics for age.
+
+## Recoding Variables ------------------------------------------------------
+# Use M2_3, M3_3 to create composite variable of scimedia
+
+# First, clean M2_3 and M3_3 individually using mutate()
+# Next, do this with mutate() and across()
+# Calculate reliability between the two items (cor_test())
+# Create composite variable and write back into data frame to save it
+
+[write code here]
--- COMM-7370/7370_15-Mar_in-class.R
+++ COMM-7370/7370_15-Mar_in-class.R
@@ -1,6 +1,7 @@
 # R4DS: Data visualization ------------------------------------------------
 # https://r4ds.hadley.nz/data-visualize.html
 
+# Install/Load packages: tidyverse, ggthemes, palmerpenguins, nycflights13
 library(tidyverse)
 library(ggthemes)
 library(palmerpenguins)
@@ -266,8 +267,9 @@ library(magrittr)
 library(summarytools)
 library(rstatix)
 library(psych)
+
 # Setting working directory
-setwd("C:/Users/SaraK/...") # Needs to be set
+setwd("C:/Users/sarak/Documents/_git-repos/sarakyeo.github.io/COMM-7370")
 
 
 # Data management tutorial ------------------------------------------------
@@ -288,26 +290,30 @@ setwd("C:/Users/SaraK/...") # Needs to be set
 
 
 ## Reading Data into R -----------------------------------------------------
-rawdata <- read_csv("7800_practice-data.csv")
+rawdata <- read_csv("7370_practice-data.csv")
 # Cleaning data
 rawdata %>%
-        freq(gc)
+  freq(gc)
+
 wdata <- rawdata %>%
-        filter(gc == 1) # Explain gc--we only want R for whom gc = 1
+  filter(gc == 1) # Explain gc--we only want R for whom gc = 1
 
 
 ## Types of Data and Levels of Measurement ---------------------------------
 # What is C1? Where do I find info about C1? Why do I need to do this?
 wdata %>%
-        freq(C1)
+  freq(C1)
+
 wdata %>%
-        freq(V1) # Where do I find info re: V1? What type of variable is it?
+  freq(V1) # Where do I find info re: V1? What type of variable is it?
 # Calculate age of respondents. Why is this important?
+
 wdata <- wdata %>%
-        rowwise() %>%
-        mutate(age = 2021-V1)
+  rowwise() %>%
+  mutate(age = 2021 - V1)
+
 wdata %>%
-        freq(age) # continuous variable; ratio
+  freq(age) # continuous variable; ratio
 
 
 ## Missing Data ------------------------------------------------------------
@@ -317,46 +323,56 @@ wdata %>%
 ## Descriptive Statistics --------------------------------------------------
 # Uses summarytools package
 wdata %>%
-        descr(age)
+  descr(age)
 
 ## Recoding Variables ------------------------------------------------------
 # M2_3, M3_3 to create composite variable of scimedia
 wdata %>%
-        freq(M2_3) # -99, "A lot 10," and "None 0" need to be recoded
+  freq(M2_3) # -99, "A lot 10," and "None 0" need to be recoded
+
 typeof(wdata$M2_3) # "character"
+
 wdata <- wdata %>%
-        mutate(cM2_3 = case_when(M2_3 == "None 0" ~ 0,
-                                 M2_3 == "1" ~ 1,
-                                 M2_3 == "2" ~ 2,
-                                 M2_3 == "3" ~ 3,
-                                 M2_3 == "4" ~ 4,
-                                 M2_3 == "5" ~ 5,
-                                 M2_3 == "6" ~ 6,
-                                 M2_3 == "7" ~ 7,
-                                 M2_3 == "8" ~ 8,
-                                 M2_3 == "9" ~ 9,
-                                 M2_3 == "A lot  10" ~ 10,
-                                 M2_3 == "-99" ~ NA_real_)) # vs NA_character_
+  mutate(cM2_3 = case_when(
+    M2_3 == "None 0" ~ 0,
+    M2_3 == "1" ~ 1,
+    M2_3 == "2" ~ 2,
+    M2_3 == "3" ~ 3,
+    M2_3 == "4" ~ 4,
+    M2_3 == "5" ~ 5,
+    M2_3 == "6" ~ 6,
+    M2_3 == "7" ~ 7,
+    M2_3 == "8" ~ 8,
+    M2_3 == "9" ~ 9,
+    M2_3 == "A lot  10" ~ 10,
+    M2_3 == "-99" ~ NA_real_
+  )) # vs NA_character_
 wdata %>%
-        freq(cM2_3)
+  freq(cM2_3)
 
 wdata %>%
-        freq(M3_3) # "A lot 10" and "None 0" need to be recoded (character)
+  freq(M3_3) # "A lot 10" and "None 0" need to be recoded (character)
+
 typeof(wdata$M3_3) # "character"
+
 wdata <- wdata %>%
-        mutate(cM3_3 = case_when(M3_3 == "None  0" ~ 0,
-                                 M3_3 == "1" ~ 1,
-                                 M3_3 == "2" ~ 2,
-                                 M3_3 == "3" ~ 3,
-                                 M3_3 == "4" ~ 4,
-                                 M3_3 == "5" ~ 5,
-                                 M3_3 == "6" ~ 6,
-                                 M3_3 == "7" ~ 7,
-                                 M3_3 == "8" ~ 8,
-                                 M3_3 == "9" ~ 9,
-                                 M3_3 == "A lot  10" ~ 10))
+  mutate(cM3_3 = case_when(
+    M3_3 == "None  0" ~ 0,
+    M3_3 == "1" ~ 1,
+    M3_3 == "2" ~ 2,
+    M3_3 == "3" ~ 3,
+    M3_3 == "4" ~ 4,
+    M3_3 == "5" ~ 5,
+    M3_3 == "6" ~ 6,
+    M3_3 == "7" ~ 7,
+    M3_3 == "8" ~ 8,
+    M3_3 == "9" ~ 9,
+    M3_3 == "A lot  10" ~ 10
+  ))
+
 wdata %>%
-        freq(cM3_3)
+  freq(cM3_3)
 
-
-
+# Do this with mutate() and across()
+# Calculate reliability between the two items (cor_test())
+# Create composite variable and write back into data frame to save it
