262f28c3f841867235d5bf9fcb367adb7ab0d491
Sara K Yeo <sarakyeo@gmail.com>
2026-01-20 11:48:09 -0700

    7370 week 4

--- COMM-7370/7370-week-4.R
+++ COMM-7370/7370-week-4.R
@@ -1,23 +1,16 @@
 # Loading packages ---------
 library(tidyverse)
-library(weights)
 library(rstatix)
 library(summarytools)
 library(psych)
-library(jtools)
-library(interactions)
-library(stargazer)
 library(huxtable)
-library(gtsummary)
-library(kableExtra)
-library(gridExtra)
-library(gtsummary)
-library(labelled)
-library(srvyr)
-library(tidyLPA)
-library(factoextra)
-library(FactoClass)
 
 # Read data into R ---------------
 gss <- read_csv(here::here("COMM-3710", "LA", "GSS.csv"))
 glimpse(gss)
+
+# Discuss data types
+
+# Descriptive statistics --------------
+gss |> freq(childs) # what is the issue? Does it need wrangling?
+# What kind of data is this? What type of data do we need if we want to calculate M, SD?
