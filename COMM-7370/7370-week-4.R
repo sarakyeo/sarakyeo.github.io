6c98453218a335cdf8e9a22040c6e14e6e53c877
Sara K Yeo <sarakyeo@gmail.com>
2026-02-04 16:51:27 -0700

    7370 class week 4-5

--- COMM-7370/7370-week-4.R
+++ COMM-7370/7370-week-4.R
@@ -1,4 +1,9 @@
 # Loading packages ---------
+# install.packages("tidyverse")
+# install.packages("rstatix")
+# install.packages("summarytools")
+# install.packages("here")
+
 library(tidyverse)
 library(rstatix)
 library(summarytools)
@@ -10,5 +15,38 @@ glimpse(gss)
 # Discuss data types
 
 # Descriptive statistics --------------
+
 gss |> freq(childs) # what is the issue? Does it need wrangling?
+
 # What kind of data is this? What type of data do we need if we want to calculate M, SD?
+
+gss |> freq(year)
+
+gss <- gss |> 
+  mutate(
+    nchild = case_when(
+        childs == "0" ~ 0,
+        childs == "1" ~ 1,
+        childs == "2" ~ 2,
+        childs == "3" ~ 3,
+        childs == "4" ~ 4,
+        childs == "5" ~ 5,
+        childs == "6" ~ 6,
+        childs == "7" ~ 7,
+        childs == "8 or more" ~ 8
+    )
+  )
+
+gss |> freq(nchild)
+
+gss |> descr(nchild) #descr is the functino for descrtiptive stats
+
+gss <- gss |> 
+  mutate(
+    dchild = case_when(
+        nchild <= 1 ~ "low",
+        nchild > 1 ~ "high"
+    )
+  )
+
+gss |> freq(dchild)
